79ef74a2e859172726a4c04b0ea7a1684e65f0c4
talha-bin-irshad <talhaarshaad5@gmail.com>
2024-05-16 19:32:12 +0500

    talha/v1-f-dev - CORS error was coming due to credentials

--- src/axiosInstance/index.ts
+++ src/axiosInstance/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  withCredentials: true,
+  withCredentials: false, // indicates whether or not cross-site Access-Control requests
+  // should be made using credentials
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
